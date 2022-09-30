d68ebf19984dea8dacfbd621236c4e11dd6b0bf5
Achira Dias <achira828@gmail.com>
2022-09-30 07:43:28 +0530

    Update items.js

--- routes/items.js
+++ routes/items.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get("/", ItemController.getAllItems);
 router.get("/:id", ItemController.getItemById);
+router.put("/set-bid/:id", ItemController.setBidById);
 
 module.exports = router;
