529ca68e481abd16c00de404c3b547b7fc3f0fba
Supun Dasanayaka <supun.19@cse.mrt.ac.lk>
2022-11-16 14:38:02 +0530

    route updated

--- routes/OfficerUsers.js
+++ routes/OfficerUsers.js
@@ -3,16 +3,16 @@ const producerController = require('../controllers/producer')
 const dataEntryController = require('../controllers/dataEntry')
 const { DistrictController } = require('../controllers/districts')
 
-router.get('/', dataEntryController.getAllDataEntry)
-router.post('/', dataEntryController.addDataEntry)
-router.put('/', dataEntryController.updateDataEntry)
-router.delete('/:id', dataEntryController.deleteDataById)
+router.get('/getAllDataEntry', dataEntryController.getAllDataEntry)
+router.post('/addDataEntry', dataEntryController.addDataEntry)
+router.put('/updateDataEntry', dataEntryController.updateDataEntry)
+router.delete('/deleteDataById/:id', dataEntryController.deleteDataById)
 
-router.get('/', producerController.getAllProducers)
-router.post('/', producerController.addNewProducer)
+router.get('/getAllProducers', producerController.getAllProducers)
+router.post('/addNewProducer', producerController.addNewProducer)
 router.get('/getByUserId', producerController.getUserById)
 router.delete('/deleteById/:id/:userId', producerController.deleteById)
 
-router.get('/', DistrictController.getAllDistricts)
+router.get('/getAllDistricts', DistrictController.getAllDistricts)
 
 module.exports = router
