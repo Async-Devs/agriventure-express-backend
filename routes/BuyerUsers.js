f9a806170bb4891faf3826265960b06636944f6a
Achira Dias <achira828@gmail.com>
2022-11-19 15:58:01 +0530

    Update BuyerUsers.js

--- routes/BuyerUsers.js
+++ routes/BuyerUsers.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const refundRequestController = require('../controllers/refundRequest')
 const refundRequestMessageController = require('../controllers/refundRequestMessage')
+const ItemController = require('../controllers/item')
 
 router.post('/addRefundRequest', refundRequestController.addRefundRequest)
 router.post('/addRefundRequestMessage', refundRequestMessageController.addRefundRequestMessage)
@@ -10,4 +11,6 @@ router.get('/refundRequestForOrder/:id', refundRequestController.getRefundReques
 router.put('/addRefundRequestMessage', refundRequestController.updateRefundRequest)
 router.put('/withdrawRefundRequest', refundRequestController.withdrawRefundRequest)
 router.put('/sendRefundRequestToOfficer', refundRequestController.sendToOfficer)
+router.put('/items/set-bid/:id', ItemController.setBidById)
+
 module.exports = router
