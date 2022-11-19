ffc87cb7fbdb1d013f001223aa62ec6ae0409286
Achira Dias <achira828@gmail.com>
2022-11-19 16:46:24 +0530

    Update order.js

--- controllers/order.js
+++ controllers/order.js
@@ -1,42 +1,39 @@
 const { Order } = require('../models/Order')
-const jwt = require("jsonwebtoken");
-const {SupportRequest} = require("../models/supportRequest");
 
-class OrderController {
-  static async getAllOrders (req, res) {
+const getAllOrders = async(req, res) => {
     console.log('fetching All Orders')
     const orderArray = await Order.find().populate('item').populate('buyer').populate('producer')
     return res.send(orderArray)
   };
 
-  static async getAllActiveOrders (req, res) {
+const getAllActiveOrders = async(req, res) => {
     console.log('fetching All Orders')
     const orderArray = await Order.find({ order_status: 'ACTIVE' }).populate('item').populate('buyer').populate('producer')
     return res.send(orderArray)
   };
 
-  static async getOrdersByBuyerId (req, res) {
+const getOrdersByBuyerId = async(req, res) => {
     const buyerId = req.params.id
     console.log('get orders for buyer', buyerId)
     const orderArray = await Order.find({ buyer: buyerId }).populate('item').populate('buyer').populate('producer')
     return res.send(orderArray)
   };
 
-  static async getOrdersByProducerId (req, res) {
+const getOrdersByProducerId = async(req, res) => {
     const producerId = req.params.id
     console.log('get orders for producer', producerId)
     const orderArray = await Order.find({ producer: producerId }).populate('item').populate('buyer').populate('producer')
     return res.send(orderArray)
   };
 
-  static async getOrderById (req, res) {
+const getOrderById = async(req, res) => {
     const orderId = req.params.id
     console.log('get order Id - ', orderId)
     const orderDetails = await Order.find({ _id: orderId }).populate('item').populate('buyer').populate('producer').populate('messages');
     console.log('get order - ', orderDetails)
     return res.send(orderDetails)
   };
-	static async updateOrderDeliveryStatus (req, res) {
+const updateOrderDeliveryStatus = async(req, res) => {
       let result = null;
       try {
         const orderId = req.params.id
@@ -58,8 +55,12 @@ class OrderController {
       }
         return  res.send(result);
 	};
-
-	
+ 
+module.exports = {
+  getAllOrders,
+  getAllActiveOrders,
+  getOrdersByBuyerId,
+  getOrdersByProducerId,
+  getOrderById,
+  updateOrderDeliveryStatus
 }
-
-module.exports.OrderController = OrderController
